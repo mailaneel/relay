c98155c64843f0312f0f9e421e68bfb5f310f85f
Anil Gullapalli <mailaneel@gmail.com>
2015-05-18 23:18:47 +1000

    added watch task as default

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,10 @@ var gulp = require('gulp');
 var babel = require('gulp-babel');
 
 gulp.task('default', function () {
+    gulp.watch('./src/**', ['build']);
+});
+
+gulp.task('build', function(){
     return gulp.src('./src/relay.js')
         .pipe(babel({optional:'runtime'}))
         .pipe(gulp.dest('./'));
