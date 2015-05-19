797928d4e640aaa48e54e318f1c60c6c669596b7
Anil Gullapalli <mailaneel@gmail.com>
2015-05-19 19:09:24 +1000

    added build as dependency for default task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 
-gulp.task('default', function () {
+gulp.task('default', ['build'], function () {
     gulp.watch('./src/**', ['build']);
 });
 
